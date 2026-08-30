b8ccc5c3b3fff294b2a320b3de4ef4072b8cb213
Farrelll <137156152+Farrelx9@users.noreply.github.com>
2026-08-30 21:24:50 +0700

    final commit for deployment stage

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const app = require("./app");
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || `https://back-end-admin-klinik.vercel.app/`;
 
 app.listen(PORT, () => {
-  console.log(`Klinik API jalan di http://localhost:${PORT}/api`);
+  console.log(
+    `Klinik API jalan di https://back-end-admin-klinik.vercel.app/api`,
+  );
 });
